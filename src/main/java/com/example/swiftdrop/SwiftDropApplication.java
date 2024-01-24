d4cab01046e5fa3b6e5c5bd1f0526b60871c6f97
Nataliya <nataliyasjmonova@gmail.com>
2024-01-24 17:12:05 +0100

    initial

---
+++ src/main/java/com/example/swiftdrop/SwiftDropApplication.java
@@ -0,0 +1,13 @@
+package com.example.swiftdrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftDropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftDropApplication.class, args);
+	}
+
+}
